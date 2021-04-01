3027fa258f934c98488a19127cbe8ac0d02f8e21
Antonio Lopes <alopes@novarumdx.com>
2021-04-01 15:44:07 +0100

    test

--- js/main.js
+++ js/main.js
@@ -126,7 +126,9 @@ function grabFrame() {
     canvas.height = imageBitmap.height;
     const ctx = canvas.getContext('2d');
     canvas.getContext('2d').drawImage(imageBitmap, 0, 0);
-    console.log('pixel:', ctx.data[100]);
+    const imagedata = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    console.log('pixel:', data[100]);
     canvas.classList.remove('hidden');
   }).catch(function(error) {
     console.log('grabFrame() error: ', error);
