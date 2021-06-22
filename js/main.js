6b949b82e9765e94fca0b54d9962db14ad8c35f7
Antonio Lopes <alopes@novarumdx.com>
2021-06-22 15:51:25 +0100

    test force portrait

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ var canvas = document.getElementById('canvas');
 var video = document.getElementById('video');
 
 torchButton.onclick = setTorch;
-window.screen.orientation.lock('landscape');
+window.screen.orientation.lock('portrait');
 window.onload = runStream();
 
 async function startCamera(){
