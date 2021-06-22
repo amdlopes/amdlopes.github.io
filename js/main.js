e18ad419109d78db89257c7949e0092e6315dc50
Antonio Lopes <alopes@novarumdx.com>
2021-06-22 20:28:43 +0100

    test rotate

--- js/main.js
+++ js/main.js
@@ -90,9 +90,9 @@ function grabFrame() {
 
     const ctx_proc = canvas_proc.getContext('2d');
     ctx_proc.save();
-    ctx_proc.translate(canvas_orig.width,0);
+    ctx_proc.translate(50,0);
     // ctx_proc.rotate(Math.PI/2);
-    ctx_proc.fillRect(1, 1, 100, 50);
+    ctx_proc.fillRect(0, 0, 100, 50);
     // ctx_proc.putImageData(imageData, 0, 0);
 
   } catch(error) {
