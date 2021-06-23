8fb52f5ff230c9b31c487af54215115710e7dd55
Antonio Lopes <alopes@novarumdx.com>
2021-06-23 10:53:18 +0100

    test rotate

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@ function setTorch() {
 async function runStream() {
   await startCamera();
   const ctx_proc = canvas_proc.getContext('2d');
-  ctx_proc.rotate(Math.PI/2);
   ctx_proc.setTransform(-1,0,0,1,canvas_proc.width,0);
+  ctx_proc.rotate(Math.PI/2);
   setInterval(grabFrame, 50);
 }
